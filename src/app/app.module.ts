92cc5d2bdaeae9cfca510765b3d45e5b9d963432
Aylin Rodriguez <aylin@nocturno.com.uy>
2023-04-30 18:33:02 +0200

    ui knob element added // export code display // ui drag and drop minor section styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,9 +20,20 @@ import { EditorFormComponent } from './modules/editor-form/editor-form.component
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    DragDropModule
+    DragDropModule,
+    HighlightModule
   ],
-  providers: [],
+  providers: [{
+    provide: HIGHLIGHT_OPTIONS,
+    useValue: {
+      fullLibraryLoader: () => import('highlight.js')
+      // coreLibraryLoader: () => import('highlight.js/lib/core'),
+      // languages: {
+      //   css: () => import('highlight.js/lib/languages/css'),
+      //   xml: () => import('highlight.js/lib/languages/xml')
+      // }
+    }
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/editor-form/editor-form.component.ts
+++ src/app/modules/editor-form/editor-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms";
 import { CdkDragEnd } from "@angular/cdk/drag-drop";
+import { SimpleChange } from '@angular/core';
 
 
 
@@ -12,16 +13,21 @@ import { CdkDragEnd } from "@angular/cdk/drag-drop";
 })
 export class EditorFormComponent implements OnInit {
 
-  checkType = function (obj) {
-    return typeof obj;
-  };
+
 
   Object = Object;
   myForm: FormGroup;
 
+
+
+
+
   constructor(private fb: FormBuilder) {
-    //console.log(this.dsForm);
     this.myForm = this.fb.group({
+      uiProperties: this.fb.group({
+        width:812,
+        height:375
+      }),
       ui: this.fb.array([]),
       groups: this.fb.array([]),
       effects: this.fb.array([])
@@ -29,8 +35,7 @@ export class EditorFormComponent implements OnInit {
 
 
 
-    //this.myForm.valueChanges.subscribe( x => this.submitForm(x))
-    //this.myForm.valueChanges.subscribe(x => console.log(x))
+    this.myForm.valueChanges.subscribe( x => this.submitForm(x))
 
   }
 
@@ -39,11 +44,7 @@ export class EditorFormComponent implements OnInit {
     return this.myForm.get('ui') as FormArray;
   }
 
-  get lessons() {
-    //const form = this.myForm;
-    
-    return this.myForm.controls["ui"] as FormArray;
-  }
+
 
 
   getPropertyList(key, parent) {
@@ -54,73 +55,130 @@ export class EditorFormComponent implements OnInit {
     return this.myForm.controls[key] as FormArray;
   }
 
+
+
   addElement(key,parent) {
-    let element: any;
-    if (key === 'button'){
-      element = this.fb.group({
-        type: key,
-        properties: this.fb.group(
-          {
-            btnX: 0,
-            btnY: 0,
-            btnText: 'text'
-          }
-        )
-      });
-    } else if (key === 'slider'){
-      element = this.fb.group({
-        type: key,
-        properties: this.fb.group(
-          {
-            sliderX: 0,
-            sliderY: 0
-          }
-        )
-      });
-    }
+    let element: any = this.fb.group({
+      type: key,
+      properties: this.getProperties(key)
+    })
     
     const array = this.sectionElements(parent);
-    //this.lessons.push(element);
     array.push(element)
     
   }
 
 
-  deleteLesson(lessonIndex: number, parent) {
-    this.sectionElements(parent).removeAt(lessonIndex);
+  getProperties(key) {
+    let properties: any;
+    if (key === 'button') {
+      properties = this.fb.group({
+        x: 0,
+        y: 0,
+        //width: 90,
+        //textSize: 16,
+        //textColor:"AA000000" ,
+        //trackForegroundColor:"CC000000" ,
+        //trackBackgroundColor:"66999999" ,
+        label:"Knob Name" ,
+        //type:"float" ,
+        minValue:0.0 ,
+        maxValue:1.0 ,
+        value:0.1 
+      })
+    } else if (key === 'slider') {
+      properties = this.fb.group({
+        sliderX: 0,
+        sliderY: 0
+      })
+    }
+    return properties
   }
 
-
-
   
-   async submitForm(x) {
-     let resultCont = document.getElementById('result');
-     let formValue = x;
-     console.log(formValue)
-     resultCont.textContent = JSON.stringify(formValue)
-     //  resultCont.textContent = `
-     //  <?xml version="1.0" encoding="UTF-8"?>
-     //  <DecentSampler minVersion="1.0.0">
+
+  deleteItem(index: number, parent) {
+    this.sectionElements(parent).removeAt(index);
+  }
+
+  dragEnd($event: CdkDragEnd) {
+    const properties = this.ui.controls[$event.source.element.nativeElement.id].controls.properties
+    const position = $event.source.getFreeDragPosition()
+    const element = $event.source.element.nativeElement
     
-     //  </DecentSampler>
-     //  `
-   }
+
+    properties.patchValue({
+      x: Math.round(position.x + element.offsetLeft),
+      y: Math.round(position.y + element.offsetTop)
+    })
+  }
+
+  checkType(key) {
+    return typeof key;
+  }
+  debugger(){
+    debugger
+  }
+  
+  async submitForm(x) {
+    let resultCont = document.getElementById('result');
+    let formValue = x;
+    console.log(x)
+    resultCont.textContent = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <DecentSampler minVersion="1.0.0">
+      <ui width="${formValue.uiProperties.width}" height="${formValue.uiProperties.height}" layoutMode="relative" bgMode="top_left">
+      <tab name="main"> 
+        ${formValue.ui.map(element => {
+          const el = `<labeled-knob 
+                        x="${element.properties.x}" 
+                        y="${element.properties.y}" 
+                        width="90" 
+                        textSize="16" 
+                        textColor="AA000000" 
+                        trackForegroundColor="CC000000" 
+                        trackBackgroundColor="66999999" 
+                        label="${element.properties.label}" 
+                        type="float" 
+                        minValue="${element.properties.minValue}" 
+                        maxValue="${element.properties.maxValue}" 
+                        value="${element.properties.value}">
+                        <binding type="amp" level="instrument" position="0" parameter="ENV_ATTACK" />
+                      </labeled-knob>
+          `
+        return el
+        }).join('')}
+      </tab>
+      </ui>
+      <groups attack="0.000" decay="25" sustain="1.0" release="0.430" volume="-3dB">
+        <group>
+          <!-- <sample loNote="21" hiNote="21" rootNote="21" path=""
+                  length="805888"/> -->
+        </group>
+      </groups>
+      <effects>
+        <effect type="lowpass" frequency="22000.0"/>
+        <effect type="chorus"  mix="0.0" modDepth="0.2" modRate="0.2" />
+        <effect type="reverb" wetLevel="0.5"/>
+      </effects>
+      <midi>
+        <!-- This causes MIDI CC 1 to control the 4th knob (cutoff) -->
+        <cc number="1">
+          <binding level="ui" type="control" parameter="VALUE" position="3" 
+                  translation="linear" translationOutputMin="0" 
+                  translationOutputMax="1" />
+        </cc>
+      </midi>
+    </DecentSampler>
+    `
+  }
 
   ngOnInit() : void {
    this.submitForm(this.myForm.value)
-   console.log('init')
   }
 
 
-  dragEnd($event: CdkDragEnd) {
-    const element = this.ui.controls[$event.source.element.nativeElement.id].controls.properties
-    const position = $event.source.getFreeDragPosition()
-    element.controls.btnX.setValue(Math.round(position.x + $event.source.element.nativeElement.offsetLeft))
-    element.controls.btnY.setValue(Math.round(position.y + $event.source.element.nativeElement.offsetTop))
 
-
-    
-  }
 
 
 }
